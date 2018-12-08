82375fc45fd3648844a307c13d6a5e3889112a13
Greger Stolt Nilsen <gregersn@gmail.com>
2018-12-09 00:13:20 +0100

    add some more parsing

--- nccfolder.py
+++ nccfolder.py
@@ -32,8 +32,48 @@ tokens = {
     '1040': 'SMS_CENTRAL',
     '1041': 'TIMESTAMP',
     '1048': '1048',
-    '1049': '1049',
+    '1049': 'CONTINUATION',
     '1080': 'SENDER',
+    '1420': 'PIT_CLDR2_MEETING',
+    '1421': 'STARTTIME',
+    '1422': 'ENDTIME',
+    '1423': 'DESCRIPTION',
+    '1424': '1424',
+    '1425': '1425',
+    '1426': '1426',
+    '1427': '1427',
+    '1428': '1428',
+    '1430': 'PIT_CLDR2_BIRTHDAY',
+    '1431': 'BIRTHDATE',
+    '1432': '1432',
+    '1433': 'NAME',
+    '1434': '1434',
+    '1435': 'BIRTHYEAR',
+    '1436': '1436',
+    '1437': '1437',
+    '1440': 'PIT_CLDR2_CALLTO',
+    '1441': 'EVENTTIME',
+    '1442': 'PHONE',
+    '1443': 'NAME',
+    '1444': '1444',
+    '1445': '1445',
+    '1446': '1446',
+    '1447': '1447',
+    '1448': '1448',
+    '1450': 'PIT_CLDR2_MEMO',
+    '1451': 'MEMO_DATE',
+    '1452': 'MEMO_DATE_2',
+    '1453': 'MEMO',
+    '1454': '1454',
+    '1455': '1455',
+    '1456': '1456',
+    '1457': '1457',
+    '1458': '1458',
+    '1460': 'PIT_CLDR2_REMINDER',
+    '1461': 'REMINDER_TIME',
+    '1462': 'REMINDER',
+    '1463': '1463',
+        
 
 }
 
@@ -46,7 +86,7 @@ def parseentry(data):
 
     outdata = []
 
-    while len(parts) > 0:
+    while len(parts) > 1:
         code = parts.pop(0)
         data = parts.pop(0)
         token, info = parsedata(code, data)
@@ -96,20 +136,32 @@ def parsemessage(data):
     return parseentry(data)
 
 
+def parseevent(data):
+    return parseentry(data)
+
+
+def parsememo(data):
+    return parseentry(data)
+
+
 functions = {
     'CONTACT': parsecontact,
     'PIT_CALLERGROUP': parsegroup,
-    'PIT_MESSAGE_INBOX': parsemessage
+    'PIT_MESSAGE_INBOX': parsemessage,
+    'PIT_CLDR2_MEETING': parseevent,
+    'PIT_CLDR2_CALLTO': parseevent,
+    'PIT_CLDR2_BIRTHDAY': parseevent,
+    'PIT_CLDR2_MEMO': parsememo,
+    'PIT_CLDR2_REMINDER': parseevent
 }
 
 
 def parsedata(code, data):
     if code not in tokens:
         print("Unknown code: {}".format(code))
-        return
+        return code, data
 
     token = tokens[code]
-    print(token)
     if token in functions:
         return functions[token](data)
 
@@ -123,12 +175,13 @@ def parseline(line):
 
     if code not in tokens:
         print("Unknown code: {}".format(code))
-        return
+        return code, parts[1]
 
     token = tokens[code]
 
     if token not in functions: 
         print("Unknown token: {}".format(token))
+        return tokens[code], parts[1]
 
     if token in functions:
         data = functions[token](parts[1])
@@ -167,6 +220,9 @@ def convert(folder, outfile="output.json"):
 
     messages = convertfile(folder, 'Messages.ncc')
     data['messages'] = messages
+
+    calendar = convertfile(folder, 'Calendar.ncc')
+    data['calendar'] = calendar
 
     with open(outfile, 'w', encoding='utf8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
